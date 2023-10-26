4c2f88b8b6baf270bfb8fa9e8affc6de56d944be
Adam <adam.fiedko@gmail.com>
2023-10-26 15:33:49 +0200

    refactor: add dependency injection

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { CurrencyController } from '../controller/currency.controller';
+import { CurrencyService } from '../service/currency.service';
+import { CurrencyRepository } from '../repository/currency.repository';
 
 export class Routes {
   private readonly currencyController: CurrencyController;
+  private readonly currencyService: CurrencyService;
+  private readonly currencyRepository: CurrencyRepository;
 
   constructor() {
-    this.currencyController = new CurrencyController();
+    this.currencyRepository = new CurrencyRepository();
+    this.currencyService = new CurrencyService(this.currencyRepository);
+    this.currencyController = new CurrencyController(this.currencyService);
   }
 
   public registerCurrencyRoutes(): Router {
