6061436f417651c173d30adf837852c5c7e513a0
Michał Kalinowski <michal@kalinowski.one>
2023-11-08 22:13:02 +0100

    test(integration): add currency service integration tests

---
+++ test/integration/currency-service.integration.spec.ts
@@ -0,0 +1,47 @@
+import { instance, mock, when } from 'ts-mockito';
+import { Config } from '../../src/config/config';
+import {
+  Currency,
+  CurrencyRepository,
+} from '../../src/repository/currency.repository';
+import { CurrencyService } from '../../src/service/currency.service';
+import { join } from 'path';
+
+describe('Currency Service Integration Test', () => {
+  let configMock: Config;
+
+  beforeEach(() => {
+    configMock = mock(Config);
+  });
+
+  it('should return values from file', async () => {
+    // given
+    when(configMock.getCurrenciesPath()).thenReturn(
+      join(__filename, '..', '__mocks__', 'valid-currencies.json')
+    );
+    const currencyService = new CurrencyService(
+      new CurrencyRepository(instance(configMock))
+    );
+
+    // when
+    const result = await currencyService.getAllCurrencies();
+
+    // expect Currency array using jest matchers
+    result.forEach((currency) => {
+      expect(Object.values(Currency).includes(currency)).toEqual(true);
+    });
+  });
+
+  it('should throw when invalid file is provided', async () => {
+    // given
+    when(configMock.getCurrenciesPath()).thenReturn(
+      join(__filename, '..', '__mocks__', 'invalid-currencies.json')
+    );
+    try {
+      new CurrencyService(new CurrencyRepository(instance(configMock)));
+    } catch (e: unknown) {
+      expect(e).toBeInstanceOf(Error);
+      expect((e as Error).name).toEqual('SyntaxError');
+    }
+  });
+});
