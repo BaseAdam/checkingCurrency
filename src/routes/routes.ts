bef152283ca8321435f43cd52a888d3229145702
Michał Kalinowski <michal@kalinowski.one>
2023-11-12 22:06:49 +0100

    feat: add di to Routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,19 +1,8 @@
 import { Router } from 'express';
 import { CurrencyController } from '../controller/currency.controller';
-import { CurrencyService } from '../service/currency.service';
-import { CurrencyRepository } from '../repository/currency.repository';
-import { Config } from '../config/config';
 
 export class Routes {
-  private readonly currencyController: CurrencyController;
-  private readonly currencyService: CurrencyService;
-  private readonly currencyRepository: CurrencyRepository;
-
-  constructor() {
-    this.currencyRepository = new CurrencyRepository(new Config());
-    this.currencyService = new CurrencyService(this.currencyRepository);
-    this.currencyController = new CurrencyController(this.currencyService);
-  }
+  constructor(private readonly currencyController: CurrencyController) {}
 
   public registerCurrencyRoutes(): Router {
     const router = Router();
