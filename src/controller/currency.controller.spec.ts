ab9199f5f668b28d7335e9e944422aaf8e2cc944
Adam <adam.fiedko@gmail.com>
2023-11-19 19:57:12 +0100

    test: add unit, integraion and e2e tests

--- src/controller/currency.controller.spec.ts
+++ src/controller/currency.controller.spec.ts
@@ -1,7 +1,7 @@
 import { CurrencyService } from '../service/currency.service';
 import { CurrencyController } from './currency.controller';
 import { instance, mock, when } from 'ts-mockito';
-import { Currency } from '../repository/currency.repository';
+import { Currency, ExchangeRate } from '../repository/currency.repository';
 import { Request, Response } from 'express';
 
 describe('currency controller - unit test', () => {
@@ -15,9 +15,11 @@ describe('currency controller - unit test', () => {
     res = {
       send: jest.fn(),
     } as unknown as Response;
+    req = {
+      params: { currency: 'USD' },
+    } as unknown as Request;
     currencyController = new CurrencyController(instance(currencyServiceMock));
   });
-
   it('send currencies as a HTTP response', async () => {
     //given
     const testedCurrencies = [Currency.USD, Currency.PLN];
@@ -28,5 +30,29 @@ describe('currency controller - unit test', () => {
 
     //then
     expect(res.send).toHaveBeenCalledWith(testedCurrencies);
+  });
+
+  it('send currencies as a HTTP response', async () => {
+    //given
+    const mainCurrency = 'USD';
+    const exchangeRate: ExchangeRate = [
+      mainCurrency,
+      {
+        rates: {
+          [Currency.PLN]: 2.31,
+          [Currency.EUR]: 2.31,
+          [Currency.CHF]: 2.31,
+          [Currency.USD]: 0,
+          [Currency.GBP]: 1.23,
+        },
+      },
+    ];
+    when(currencyServiceMock.getCurrencyChangeRate(mainCurrency)).thenResolve(exchangeRate);
+
+    //when
+    await currencyController.getCurrencyChangeRate(req, res);
+
+    //then
+    expect(res.send).toHaveBeenCalledWith(exchangeRate);
   });
 });
