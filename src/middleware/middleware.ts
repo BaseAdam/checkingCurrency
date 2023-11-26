8c799dfff5c53a9f1adccccb602efc846d664f39
Adam <adam.fiedko@gmail.com>
2023-11-26 19:59:15 +0100

    refactor: make proper middleware and adjust output to be a proper JSON

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 
+export type ValidationMiddlewareFunc = (data: { body: unknown; params: unknown; headers: unknown }) => void;
 export class ValidationMiddleware {
-  public getMiddleware<T>(validate: (currency: string | undefined) => T) {
+  public getMiddleware(validate: ValidationMiddlewareFunc) {
     return (req: Request, res: Response, next: NextFunction): void | Error => {
       try {
-        validate(req.params.currency);
+        validate({
+          body: req.body,
+          params: req.params,
+          headers: req.headers,
+        });
       } catch (error) {
         res.status(400).send(error.message);
         return;
