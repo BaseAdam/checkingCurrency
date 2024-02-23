091279a35bac92372acbe1cb0af34155919c562f
Adam <adam.fiedko@gmail.com>
2024-02-23 14:22:53 +0100

    test: add test containers and fix tests

--- src/repository/currency.repository.spec.ts
+++ src/repository/currency.repository.spec.ts
@@ -1,29 +1,30 @@
 import { ComparisonRate, CurrencyEntity, CurrencyRepository, ExchangeRate } from './currency.repository';
 import { when, instance, mock, anything } from 'ts-mockito';
 import { Currency } from './currency.repository';
-import { Collection, FindCursor } from 'mongodb';
+import { Collection, FindCursor, ObjectId, WithId } from 'mongodb';
 
 describe('currency repository - unit test', () => {
   let currencyRepository: CurrencyRepository;
   let collectionMock: Collection<CurrencyEntity>;
-  let findCursorMock: FindCursor<CurrencyEntity>;
+  let findCursorMock: FindCursor<WithId<CurrencyEntity>>;
+  let idMock: ObjectId;
 
   beforeEach(() => {
     collectionMock = mock(Collection<CurrencyEntity>);
-    findCursorMock = mock(FindCursor<CurrencyEntity>);
+    findCursorMock = mock(instance(FindCursor<CurrencyEntity>));
+    idMock = mock(ObjectId);
     currencyRepository = new CurrencyRepository(instance(collectionMock));
   });
 
   it('should return values from db', async () => {
     //given
     const currencyNames = ['USD', 'PLN'];
-    const currencyEntity: CurrencyEntity[] = [
-      { name: 'USD', rates: { PLN: 3.77, EUR: 0.89, GBP: 0.79, CHF: 0.99 } },
-      { name: 'PLN', rates: { USD: 0.27, EUR: 0.24, GBP: 0.21, CHF: 0.26 } },
+    const currencyEntity: WithId<CurrencyEntity>[] = [
+      { _id: idMock, name: 'USD', rates: { PLN: 3.77, EUR: 0.89, GBP: 0.79, CHF: 0.99 } },
+      { _id: idMock, name: 'PLN', rates: { USD: 0.27, EUR: 0.24, GBP: 0.21, CHF: 0.26 } },
     ];
     when(findCursorMock.toArray()).thenResolve(currencyEntity);
-    when(collectionMock.find()).thenReturn(findCursorMock);
-    when(currencyEntity.map(anything())).thenReturn(['USD', 'PLN']);
+    when(collectionMock.find()).thenReturn(instance(findCursorMock));
     //when
     const result = await currencyRepository.getAllCurrencies();
 
@@ -34,7 +35,7 @@ describe('currency repository - unit test', () => {
   it('should return exchange rate of given currency', async () => {
     //given
     const mainCurrency = Currency.USD;
-    const currencyEntity = { name: 'USD', rates: { PLN: 3.77, EUR: 0.89, GBP: 0.79, CHF: 0.99 } };
+    const currencyEntity: WithId<CurrencyEntity> = { _id: idMock, name: 'USD', rates: { PLN: 3.77, EUR: 0.89, GBP: 0.79, CHF: 0.99 } };
     const exchangeRates: ExchangeRate[] = [
       { currency: Currency.PLN, exchangeRate: 3.77 },
       { currency: Currency.EUR, exchangeRate: 0.89 },
@@ -54,6 +55,7 @@ describe('currency repository - unit test', () => {
     const mainCurrency = Currency.USD;
     const currencyToCompare = Currency.EUR;
     const exchangeRate: ComparisonRate = { exchangeRate: 0.89 };
+    const currencyEntity: WithId<CurrencyEntity> = { _id: idMock, name: 'USD', rates: { PLN: 3.77, EUR: 0.89, GBP: 0.79, CHF: 0.99 } };
     when(collectionMock.findOne(anything())).thenResolve(currencyEntity);
     //when
     const result = await currencyRepository.getCurrencyComparison(mainCurrency, currencyToCompare);
---
+++ test/integration/currency-repository.integration.spec.ts
@@ -0,0 +1,63 @@
+import { GenericContainer, StartedTestContainer } from 'testcontainers';
+import { Collection } from 'mongodb';
+import { MongoDatabase } from '../../src/mongo-database';
+import { ComparisonRate, Currency, CurrencyEntity, CurrencyRepository, ExchangeRate } from '../../src/repository/currency.repository';
+
+describe('Currency Repository Integration Test', () => {
+  const mainCurrency = Currency.USD;
+  const currencyToCompare = Currency.CHF;
+  let mongoContainer: StartedTestContainer;
+  let mongoDatabase: MongoDatabase;
+  let collection: Collection<CurrencyEntity>;
+
+  beforeAll(async () => {
+    mongoContainer = await new GenericContainer('mongo').withExposedPorts(27017).start();
+    const uri = `mongodb://localhost:${mongoContainer.getMappedPort(27017)}`;
+    mongoDatabase = await MongoDatabase.connect(uri);
+    collection = mongoDatabase.getCollection('Currencies') as unknown as Collection<CurrencyEntity>;
+  });
+
+  it('should return values from db', async () => {
+    // given
+    const currencyNames = ['USD', 'PLN', 'EUR', 'GBP', 'CHF'];
+    const currencyRepository = new CurrencyRepository(collection);
+
+    // when
+    const result = await currencyRepository.getAllCurrencies();
+
+    // expect Currency array using jest matchers
+    expect(result).toEqual(currencyNames);
+  });
+
+  it('should return exchange rate of given currency', async () => {
+    // given
+    const exchangeRates: ExchangeRate[] = [
+      { currency: Currency.PLN, exchangeRate: 3.77 },
+      { currency: Currency.EUR, exchangeRate: 0.89 },
+      { currency: Currency.GBP, exchangeRate: 0.79 },
+      { currency: Currency.CHF, exchangeRate: 0.99 },
+    ];
+    const currencyRepository = new CurrencyRepository(collection);
+    // when
+    const result = await currencyRepository.getCurrencyChangeRate(mainCurrency);
+
+    // expect Currency array using jest matchers
+    expect(result).toStrictEqual(exchangeRates);
+  });
+
+  it('should return exchange rate of currency comparison', async () => {
+    //given
+    const exchangeRate: ComparisonRate[] = [{ exchangeRate: 0.99 }];
+    const currencyRepository = new CurrencyRepository(collection);
+    //when
+    const result = await currencyRepository.getCurrencyComparison(mainCurrency, currencyToCompare);
+
+    //then
+    expect([result]).toStrictEqual(exchangeRate);
+  });
+
+  afterAll(async () => {
+    await mongoDatabase.stop();
+    await mongoContainer.stop();
+  });
+});
