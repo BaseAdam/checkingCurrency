80a6f5fb69e3a6c6e24de6135284ff395df015b7
Adam <adam.fiedko@gmail.com>
2025-04-08 21:04:27 +0200

    feat: add swagger and generate openAPI docs

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,8 +13,60 @@ export class Routes {
 
   public registerRoutes(): Router {
     const router = Router();
-
+    /**
+     * @openapi
+     * /api/currency:
+     *   get:
+     *     description: Get all currencies
+     *     responses:
+     *       200:
+     *         description: Returns a list of all currencies as an array
+     *         content:
+     *           application/json:
+     *             example:
+     *                 - "EUR"
+     *                 - "GBP"
+     */
     router.get('/currency', (req, res) => this.currencyController.getAllCurrencies(req, res));
+    /**
+     * @openapi
+     * /api/currency/{currency}:
+     *   get:
+     *     description: Get exchange rate pair for specific currencies
+     *     parameters:
+     *       - in: path
+     *         name: currency
+     *         required: true
+     *         schema:
+     *           type: string
+     *         example: USD
+     *         description: The base currency code (e.g., USD)
+     *       - in: query
+     *         name: compare_to
+     *         required: false
+     *         schema:
+     *           type: string
+     *         example: EUR
+     *         description: The currency to compare against (e.g., EUR)
+     *     responses:
+     *       200:
+     *         description: Exchange rate pair for the given currencies
+     *         content:
+     *           application/json:
+     *             examples:
+     *               withCompareTo:
+     *                 summary: Without `compare_to` query parameter
+     *                 value:
+     *                   exchangeRates:
+     *                     - currency: "EUR"
+     *                       exchangeRate: 0.91
+     *                     - currency: "GBP"
+     *                       exchangeRate: 0.23
+     *               withoutCompareTo:
+     *                 summary: With `compare_to` query parameter
+     *                 value:
+     *                     exchangeRate: 1.20
+     */
     router.get(
       '/currency/:currency',
       (req, res, next) => {
